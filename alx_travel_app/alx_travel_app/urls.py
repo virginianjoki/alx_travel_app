80dcb8c067c00ba6b8b9e3117e8decbc32e5b739
virginianjoki <njokivirginia0619@gmail.com>
2025-07-19 18:21:22 +0300

    commit

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -32,7 +32,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger',cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include('listings.urls')),
 ]
